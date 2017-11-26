e4b00ef0ede654448d804aa28b1f859bda73e47d
ffrankies <wanyef@mail.gvsu.edu>
2017-11-26 17:49:56 -0500

    Now showing total status for all processes

--- src/components/StatusDisplay.js
+++ src/components/StatusDisplay.js
@@ -19,15 +19,7 @@ class StatusDisplay extends Component {
             const pageTables = swapSpaceKeys.map((key) => this.props.swapSpace[key]);
             const pages = pageTables.map((pageTable) => 
                 <li className='list-group-item p-1' key={pageTable.processNumber.toString()}>
-                    <div className='row w-100'>
-                        <div className='col col-sm-3'>{pageTable.processNumber}</div>
-                        <div className='col col-sm-5'> 
-                            References <span className='badge badge-dark'>{pageTable.numReferences}</span>
-                        </div>
-                        <div className='col col-sm-4'>
-                            Faults <span className='badge badge-dark'>{pageTable.numPageFaults}</span>
-                        </div>
-                    </div>
+                    {this.renderRow(pageTable.processNumber, pageTable.numReferences, pageTable.numPageFaults)}
                 </li>
             );
             pageList = <ul className='list-group list-group-flush'>{pages}</ul>;
@@ -36,9 +28,44 @@ class StatusDisplay extends Component {
     };
 
     /**
-     * Render the total status in the card footer
+     * Renders the total status in the card footer
      * @return {div} total
      */
+    renderTotalStatus() {
+        const swapSpaceKeys = Object.keys(this.props.swapSpace);
+        const pageTables = swapSpaceKeys.map((key) => this.props.swapSpace[key]);
+        var totalReferences = 0, totalFaults = 0;
+        pageTables.forEach((pageTable) => {
+            totalReferences += pageTable.numReferences;
+            totalFaults += pageTable.numPageFaults;
+        });
+        return (
+            <div className='card-footer text-muted p-0'>
+                {this.renderRow('Total', totalReferences, totalFaults)}
+            </div>
+        );
+    };
+    
+    /**
+     * Renders a row containing three columns.
+     * @param {string} id - either the process number, or a text identifier
+     * @param {int} references - the number of references for the process / identifier
+     * @param {int} faults - the number of faults for the process / identifier
+     * @return {div} row - the row containing the provided information
+     */
+    renderRow(id, references, faults) {
+        return (
+            <div className='row'>
+                <div className='col col-sm-3'>{id}</div>
+                <div className='col col-sm-5'> 
+                    References <span className='badge badge-dark'>{references}</span>
+                </div>
+                <div className='col col-sm-4'>
+                    Faults <span className='badge badge-dark'>{faults}</span>
+                </div>
+            </div>
+        );
+    };
 
     /**
      * Renders the page table. The page table is to passed to it in props.
@@ -49,6 +76,7 @@ class StatusDisplay extends Component {
                 <div className='card text-center'>
                     <div className='card-header'>Status</div>
                     {this.renderStatus()}
+                    {this.renderTotalStatus()}
                 </div>
             </div>
         );
