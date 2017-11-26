04601f80e35df7ed2f82e2a40e7c210c903a8f27
ffrankies <wanyef@mail.gvsu.edu>
2017-11-26 15:22:51 -0500

    Process number changed to an integer

--- src/utils/buttonControl.js
+++ src/utils/buttonControl.js
@@ -24,9 +24,7 @@ export function nextReference() {
         alert('Reached the last reference - cannot move further');
         return;
     }
-    const memReference = this.props.memReferences[currentReference];
-    var [processNum, pageNum] = memReference.split(':');
-    pageNum = parseInt(pageNum, 2);
+    const [processNum, pageNum] = splitReference(this.props.memReferences, currentReference);
     const [swapSpace, frameTable] = updateTables(this.state.frameTable, this.state.swapSpace, processNum, pageNum);
     this.setState({ 
         currentReference : currentReference,
@@ -38,12 +36,28 @@ export function nextReference() {
 };
 
 /**
+ * Splits the reference string into a process number and a page number.
+ * @param {array} memReferences - A list of reference strings of the form "Proc#:    Page#"
+ * @param {number} referenceNumber - The index of the current reference string
+ * @return {array} [process number, page number]
+ */
+function splitReference(memReferences, referenceNumber) {
+    const memReference = memReferences[referenceNumber];
+    var [processNumber, pageNumber] = memReference.split(':');
+    processNumber = processNumber.substring(1);
+    processNumber = parseInt(processNumber, 10);
+    pageNumber = parseInt(pageNumber, 2);
+    return [processNumber, pageNumber]
+};
+
+/**
  * Updates the frame table and the page table for the current process.
+ * @since v0.0.1
  * @param {FrameTable} frameTable - the frame table for this run
  * @param {object} swapSpace - the swap space for this run, contains all the page tables
  * @param {string} processNumber - the current process' number
  * @param {number} pageNumber - the current page's number
- * @since v0.0.1
+ * @return {array} [updated swap space, updated frame table]
  */
 function updateTables(frameTable, swapSpace, processNumber, pageNumber) {
     const [updatedPageTable, updatedSwapSpace] = getPageTable(processNumber, swapSpace);
@@ -56,13 +70,20 @@ function updateTables(frameTable, swapSpace, processNumber, pageNumber) {
     return [updatedSwapSpace, updatedFrameTable];
 };
 
-function getPageTable(processNum, swapSpace) {
+/**
+ * Updates the frame table and the page table for the current process.
+ * @since v0.0.1
+ * @param {string} processNumber - the current process' number
+ * @param {object} swapSpace - the swap space for this run, contains all the page tables
+ * @return {array} [updated page table, updated swap space]
+ */
+function getPageTable(processNumber, swapSpace) {
     const newSwapSpace = swapSpace;
     var pageTable;
-    if (!(processNum in newSwapSpace)) {
-        newSwapSpace[processNum] = new PageTable(processNum);
+    if (!(processNumber in newSwapSpace)) {
+        newSwapSpace[processNumber] = new PageTable(processNumber);
     }
-    pageTable = newSwapSpace[processNum];
+    pageTable = newSwapSpace[processNumber];
     return [pageTable, newSwapSpace];
 };
 
